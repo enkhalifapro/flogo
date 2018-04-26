2de0a1dafa64898aa761e6c079ca4a98fb25031e
ayman <enkhalifapro@gmail.com>
2018-04-26 08:26:25 +0200

    Finish delta calculations

--- activity/calculatedelta/activity.go
+++ activity/calculatedelta/activity.go
@@ -1,9 +1,12 @@
 package calculatedelta
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/tidwall/buntdb"
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -23,32 +26,105 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
-
 	// do eval
+	// read first 2 messages by type
+	// Open the data.db file. It will be created if it doesn't exist.
+	db, err := buntdb.Open("msgq.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	isSame := reflect.DeepEqual(nil, nil)
-	if !isSame {
-		context.SetOutput("isSame", false)
-		context.SetOutput("changes", nil)
+	// register query indexes
+
+	// query
+	msgType := context.GetInput("messageType").(string)
+	microserviceName := context.GetInput("microserviceName").(string)
+
+	msgs := make(map[string]string)
+	db.CreateIndex("index", "*", buntdb.IndexJSON("createdAt"), buntdb.IndexJSON("type"), buntdb.IndexJSON("microservice"))
+	//db.CreateIndex("type", "*", )
+	err = db.View(func(tx *buntdb.Tx) error {
+		fmt.Println("in 1")
+		err := tx.AscendGreaterOrEqual("index", `{"type":"`+msgType+`","microservice":"`+microserviceName+`"}`, func(key, value string) bool {
+			if len(msgs) < 2 {
+				msgs[key] = value
+			}
+			return true
+		})
+		return err
+	})
+	if err != nil {
+		return false, err
+	}
+
+	// if no new messages
+	if len(msgs) < 1 {
+		context.SetOutput("hasDelta", false)
+		context.SetOutput("delta", "")
 		return true, nil
 	}
+
+	// if it first message send all data
+	if len(msgs) == 1 {
+		context.SetOutput("hasDelta", true)
+		for _, msg := range msgs {
+			context.SetOutput("delta", msg)
+		}
+		return true, nil
+	}
+
+	// if we have 2 messages then calculate deltas
+
 	return true, nil
 }
 
 // CalculateDeltas get changes of 2 dictionaries
-func CalculateDeltas(prev map[string]string, current map[string]string) map[string]string {
-	diffs := make(map[string]string)
+func CalculateDeltas(prev map[string]interface{}, current map[string]interface{}) map[string]interface{} {
+	diffs := make(map[string]interface{})
 
 	// updated & deleted fields
 	for pKey, pVal := range prev {
-		cVal, ok := current[pKey]
-		if ok && pVal != cVal {
-			// field updated
-			diffs[pKey] = cVal
-		} else {
-			// field deleted
-			diffs[pKey] = "null"
+
+		if _, ok := pVal.(string); ok { // string val
+			cVal, ok := current[pKey]
+			if ok && pVal != cVal {
+				// field updated
+				diffs[pKey] = cVal.(string)
+			}
+			if !ok {
+				// field deleted
+				diffs[pKey] = "null"
+			}
+		} else if val, ok := pVal.(map[string]interface{}); ok { // object val
+			cVal, isExist := current[pKey]
+			if isExist && !reflect.DeepEqual(pVal, cVal) {
+				// field updated
+				itemDiff := CalculateDeltas(val, current[pKey].(map[string]interface{}))
+				diffs[pKey] = itemDiff
+			}
+			if !isExist {
+				// field deleted
+				diffs[pKey] = "null"
+			}
+
+		} else { // if array val
+			cVal, ok := current[pKey]
+			if ok && !reflect.DeepEqual(pVal, cVal) {
+				itemDiffs := make([]interface{}, 0)
+				for i := 0; i < len(pVal.([]interface{})); i++ {
+					// field updated
+					itemDiff := CalculateDeltas(pVal.([]interface{})[i].(map[string]interface{}), cVal.([]interface{})[i].(map[string]interface{}))
+					itemDiffs = append(itemDiffs, itemDiff)
+				}
+				diffs[pKey] = itemDiffs
+			}
+			if !ok {
+				// field deleted
+				diffs[pKey] = "null"
+			}
 		}
+
 	}
 
 	// created fields
--- activity/calculatedelta/activity_test.go
+++ activity/calculatedelta/activity_test.go
@@ -1,6 +1,7 @@
 package calculatedelta
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"testing"
 
@@ -37,25 +38,174 @@ func TestCreate(t *testing.T) {
 
 func TestEval(t *testing.T) {
 
-	defer func() {
+	/* defer func() {
 		if r := recover(); r != nil {
 			t.Failed()
 			t.Errorf("panic during execution: %v", r)
 		}
-	}()
+	}() */
 
 	act := NewActivity(getActivityMetadata())
 	tc := test.NewTestActivityContext(getActivityMetadata())
 
 	//setup attrs
-
+	tc.SetInput("messageType", "MICROSERVICE_READ_VALUES")
+	tc.SetInput("microserviceName", "CMA")
 	act.Eval(tc)
 
 	//check result attr
 }
 
-func TestCalculateDeltas(t *testing.T) {
+func getPrevValues() map[string]interface{} {
+	str := `{
+	"devices": [{
+		"Comment": "test5",
+		"External Name": "",
+		"Firmware Version": "\"7.2/1.8\"",
+		"Model": "MA 1000 \u0026 2000",
+		"Name": "",
+		"Revision": "A00",
+		"Serial Number": "#09377B7",
+		"Type": "Master",
+		"index": "cntrl:0"
+	}, {
+		"Comment": "",
+		"DL AGC Attenuation Value": "N/A",
+		"DL AGC Status": "N/A",
+		"DL DCA Manual Override": "N/A",
+		"DL Input Power Status": "N/A",
+		"DL Power": "N/A",
+		"DL Power Interface Type": "N/A",
+		"External Name": "",
+		"Firmware Version": "N/A",
+		"Name": "BU 4                ",
+		"Revision": "N/A",
+		"Serial Number": "#FFFFFFF",
+		"UL Attenuation Value": "N/A",
+		"index": "cntrl:0#p5-base_unit#bu:1-4"
+	}],
+	"inventory": {
+		"cntrl:0": "\"7.2/1.8\"",
+		"cntrl:0#p2-riu": "N/A",
+		"cntrl:0#p2-riu#p1-btsc": "N/A",
+		"cntrl:0#p2-riu#p10-btsc": "N/A",
+		"cntrl:0#p2-riu#p11-btsc": "N/A",
+		"cntrl:0#p2-riu#p12-btsc": "N/A",
+		"cntrl:0#p2-riu#p2-btsc": "N/A",
+		"cntrl:0#p2-riu#p2-cmu": "N/A",
+		"cntrl:0#p2-riu#p3-btsc": "N/A",
+		"cntrl:0#p2-riu#p4-btsc": "N/A",
+		"cntrl:0#p2-riu#p5-btsc": "N/A",
+		"cntrl:0#p2-riu#p6-btsc": "N/A",
+		"cntrl:0#p2-riu#p7-btsc": "N/A",
+		"cntrl:0#p2-riu#p8-btsc": "N/A",
+		"cntrl:0#p2-riu#p9-btsc": "N/A",
+		"cntrl:0#p3-och:unit": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:1": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:2": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:3": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:4": "N/A",
+		"cntrl:0#p3-och_unit#och:1-4": "N/A",
+		"cntrl:0#p3-och_unit#och:5-8": "N/A",
+		"cntrl:0#p4-och:unit": "N/A",
+		"cntrl:0#p4-och:unit#och:1-4#gx:1": "N/A",
+		"cntrl:0#p4-och_unit#och:1-4": "N/A",
+		"cntrl:0#p5-base:unit": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:1": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:2": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:3": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:4": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:1": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:2": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:3": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:4": "N/A",
+		"cntrl:0#p6-och:unit": "N/A",
+		"cntrl:0#p6-och_unit#och:1-4": "N/A"
+	}
+}`
 
+	obj := make(map[string]interface{})
+	json.Unmarshal([]byte(str), &obj)
+	return obj
+}
+
+func getCurrentValues() map[string]interface{} {
+	str := `{
+	"devices": [{
+		"Comment": "test5New",
+		"External Name": "",
+		"Firmware Version": "\"7.2/1.8\"",
+		"Model": "MA 1000 \u0026 2000",
+		"Name": "",
+		"Revision": "A00",
+		"Serial Number": "#09377B7",
+		"Type": "Master",
+		"index": "cntrl:0"
+	}, {
+		"Comment": "with comment",
+		"DL AGC Attenuation Value": "N/A",
+		"DL AGC Status": "N/A",
+		"DL DCA Manual Override": "N/A",
+		"DL Input Power Status": "N/A",
+		"DL Power": "N/A",
+		"DL Power Interface Type": "N/A",
+		"External Name": "",
+		"Firmware Version": "N/A",
+		"Name": "BU 4                ",
+		"Revision": "N/A",
+		"Serial Number": "#FFFFFFF",
+		"UL Attenuation Value": "N/A",
+		"index": "cntrl:0#p5-base_unit#bu:1-4"
+	}],
+	"inventory": {
+		"cntrl:0": "\"7.2/1.9\"",
+		"cntrl:0#p2-riu": "N/A",
+		"cntrl:0#p2-riu#p1-btsc": "N/A",
+		"cntrl:0#p2-riu#p10-btsc": "N/A",
+		"cntrl:0#p2-riu#p11-btsc": "N/A",
+		"cntrl:0#p2-riu#p12-btsc": "N/A",
+		"cntrl:0#p2-riu#p2-btsc": "N/A",
+		"cntrl:0#p2-riu#p2-cmu": "N/A",
+		"cntrl:0#p2-riu#p3-btsc": "N/A",
+		"cntrl:0#p2-riu#p4-btsc": "N/A",
+		"cntrl:0#p2-riu#p5-btsc": "N/A",
+		"cntrl:0#p2-riu#p6-btsc": "N/A",
+		"cntrl:0#p2-riu#p7-btsc": "N/A",
+		"cntrl:0#p2-riu#p8-btsc": "N/A",
+		"cntrl:0#p2-riu#p9-btsc": "N/A",
+		"cntrl:0#p3-och:unit": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:1": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:2": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:3": "N/A",
+		"cntrl:0#p3-och:unit#och:1-4#gx:4": "N/A",
+		"cntrl:0#p3-och_unit#och:1-4": "N/A",
+		"cntrl:0#p3-och_unit#och:5-8": "N/A",
+		"cntrl:0#p4-och:unit": "N/A",
+		"cntrl:0#p4-och:unit#och:1-4#gx:1": "N/A",
+		"cntrl:0#p4-och_unit#och:1-4": "N/A",
+		"cntrl:0#p5-base:unit": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:1": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:2": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:3": "N/A",
+		"cntrl:0#p5-base_unit#bu:1-4#hx:4": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:1": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:2": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:3": "N/A",
+		"cntrl:0#p5-base_unit#bu:5-8#hx:4": "N/A",
+		"cntrl:0#p6-och:unit": "N/A",
+		"cntrl:0#p6-och_unit#och:1-4": "N/A"
+	}
+}`
+	obj := make(map[string]interface{})
+	json.Unmarshal([]byte(str), &obj)
+	return obj
+}
+
+func TestCalculateDeltas(t *testing.T) {
 	defer func() {
 		if r := recover(); r != nil {
 			t.Failed()
@@ -64,23 +214,20 @@ func TestCalculateDeltas(t *testing.T) {
 	}()
 
 	//setup attrs
-	prev := make(map[string]string)
-	prev["name"] = "ayman"
-	prev["title"] = "E.L.S developer"
-	current := make(map[string]string)
-	current["name"] = "ayman hassan"
-	current["age"] = "32"
+
+	prev := getPrevValues()
+	current := getCurrentValues()
 
 	diffs := CalculateDeltas(prev, current)
 
 	//check result attr
-	if diffs["title"] != "null" {
+	if diffs["devices"].([]interface{})[0].(map[string]interface{})["Comment"] != "test5New" {
 		t.Fail()
 	}
-	if diffs["name"] != "ayman hassan" {
+	if diffs["devices"].([]interface{})[1].(map[string]interface{})["Comment"] != "with comment" {
 		t.Fail()
 	}
-	if diffs["age"] != "32" {
+	if diffs["inventory"].(map[string]interface{})["cntrl:0"] != "\"7.2/1.9\"" {
 		t.Fail()
 	}
 }
